8fd48a1708c71d1ef8b05c0ab55be30acbbff0fa
Adriaan Marain <adriaanmarain300@gmail.com>
2018-02-07 17:24:25 +0100

    class 8 through 18
    Firebase is pretty sick

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBPWqAuXnqrxiu2IjInpr_VZiurN_FWoMk",
+    authDomain: "catch-of-the-day-reactjs-tut.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-reactjs-tut.firebaseio.com",
+});
+
+export default base;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 import App from './components/App';
 
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker}/>
+                <Match exactly pattern="/store/:storeId" component={App}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
 //render(<StorePicker/>, document.querySelector('#main'));
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
